6bd577012742e8a35f5e1fedc936917d67d4cf30
dspeckman76 <d.speckman76@gmail.com>
2026-01-05 18:28:05 -0600

    Jan05 Update 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Books from './pages/Books';
+import { books } from "./data";
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" component={Books} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router> 
